01c1f83ddeefa974d297dd69e2bc49af6c315b36
Mabrouka  khalfa <khalfambrouka@gmail.com>
2021-09-08 20:27:21 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Home from './Home'
+import VoitureDetails from './VoitureDetails';
+import NavBar from './Header/NavBar'
+import Login from './User/Login'
+import Register from './User/Register'
+import UserList  from './User/UserList'
+import UserEdit from './User/UserEdit'
+import Api from './Api'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar/>
+      <Route path='/' component={Home} exact></Route>
+      <Route path='/voiture/:id' component={VoitureDetails} />
+      <Route path='/login' component={Login} />
+           <Route path='/admin/userlist' component={UserList}/>
+           <Route path='/admin/user/:id/edit' component={UserEdit} />
+           <Route path='/register' component={Register} />
+
+           <Api/>  
+
+           <div className="copyright">
+            <p className="p_copyright">Copyright © voiture. All right reserved| application devaloped by Mabrouka khalfa</p>
+        </div>  
+        </Router>
   );
 }
 
